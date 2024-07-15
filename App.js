3211b9345d91a6682ef3b4aeaf827fc240fa2f6f
SrikanthBethi9885 <srikanth.bethi@missiong3.com>
2024-07-15 11:01:07 +0530

    chat app

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import ListView from './components/ListView';
-import LoginScreen from './components/LoginScreen';
-const App=() => {
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './chat-app/Home';
+import Signup from './chat-app/Signup';
+import Login from './chat-app/Login';
+import Background from './chat-app/Background';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View >
-      {/* <ListView/> */}
-      <LoginScreen />
-</View>
+    <Background>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Background>
   );
 }
-
 
 export default App;
